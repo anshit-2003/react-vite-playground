6565c87dd706e8cc9c3903fda2420c7d48298a4c
Anshit Bakshi <84705984+anshit-2003@users.noreply.github.com>
2024-01-22 19:02:49 +0530

    Adding Component Files For Lab

---
+++ src/AddToDo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddToDo() {
+    return <></>;
+}
+
+export default AddToDo;
---
+++ src/ToDo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ToDo() {
+    return <></>;
+}
+
+export default ToDo;
